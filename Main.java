e73337342a0bacaba067c17ea6cd3e4015fdad01
Vincent <const.lomo@gmail.com>
2018-04-17 09:47:13 +0800

    add timer

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
 /**
  * Hello world!
  */
+import java.util.Timer;
+import java.util.TimerTask;
 public class Main
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("当前时间:" + this.scheduledExecutionTime());
+            }
+        }, 0, 1000);
     }
 }
